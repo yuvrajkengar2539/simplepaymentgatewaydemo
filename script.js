cf302b08cbd4dd3d5979d2f8c291c2735713fd62
yuvrajkengar2539 <162606922+yuvrajkengar2539@users.noreply.github.com>
2024-09-06 15:03:28 +0530

    Add files via upload
    added

---
+++ script.js
@@ -0,0 +1,15 @@
+function Card() {
+    window.location.href = "Card.html"; // Ensure Card.html is the correct path
+}
+function UPI() {
+    window.location.href = "UPI.html"; // Replace with actual path
+}
+function Netbanking() {
+    window.location.href = "Netbanking.html"; // Replace with actual path
+}
+function Wallet() {
+    window.location.href = "Wallet.html"; // Replace with actual path
+}
+function Transfer() {
+    window.location.href = "Transfer.html"; // Replace with actual path
+}
